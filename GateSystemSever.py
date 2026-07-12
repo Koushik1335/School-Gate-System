e51ce4b7dc122e479ba656bbffa15f2b05ed7447
Koushik1335 <168787941+Koushik1335@users.noreply.github.com>
2026-07-12 14:29:06 +1000

    Update GateSystemSever.py

--- GateSystemSever.py
+++ GateSystemSever.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-server = Flask(__name__)
+server = Flask(__name__, template_folder='.', static_folder='.', static_url_path='')
 
 @server.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('GateSystem.html')
 
 if __name__ == "__main__":
     server.run(debug=True)
